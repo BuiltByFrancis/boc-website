d2c43ea4552f4ab584b8a498dc7b14f0354fdb0a
BuiltByFrancis <builtbyfrancis@outlook.com>
2022-07-10 23:13:02 +0100

    Basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
-import "./app.css";
+import { Navbar } from "./components";
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h2 className="text-black">App</h2>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/play">Play</Link> | <Link to="/mint">Mint</Link>
-      </nav>
+      <Navbar />
       <Outlet />
     </div>
   );
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+import "./Navbar.css";
+
+const RouterLink = ({ to, title }) => {
+  return (
+    <li key={title} className="text-white/70 hover:text-white mx-2">
+      <Link to={to}>{title}</Link>
+    </li>
+  );
+};
+
+const SocialLink = ({ url }) => {
+  return (
+    <li key={url} className="text-white/70 hover:text-white mx-2">
+      <SocialIcon url={url} fgColor="#ffffff" bgColor="#0ea5e9" />
+    </li>
+  );
+};
+
+const MobileMenu = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+  return (
+    <div className="w-full flex md:hidden justify-around items-center"></div>
+  );
+};
+
+const DesktopMenu = () => {
+  return (
+    <div className="w-full md:flex hidden justify-between items-center">
+      <ul className="flex flex-row justify-between items-center text-xl">
+        {[
+          { to: "/#", title: "Home" },
+          { to: "/#about", title: "About" },
+          { to: "/#team", title: "Team" },
+          { to: "/#contact", title: "Contact" },
+        ].map((item) => (
+          <RouterLink to={item.to} title={item.title} />
+        ))}
+      </ul>
+      <a href="#" className="">
+        <img src="../../../images/logo.png" alt="Logo" />
+      </a>
+      <ul className="flex flex-row justify-between items-center text-white text-xl">
+        {[
+          "https://github.com/BuiltByFrancis",
+          "https://discord.gg/nqMmsZJW",
+          "https://twitter.com/BuiltByFrancis",
+        ].map((item) => (
+          <SocialLink url={item} />
+        ))}
+        <li className="text-white/70 hover:text-white mx-2 bg-[#0ea5e9] py-2 px-7 mx-2 rounded-full cursor-pointer ">
+          Connect Wallet
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const Navbar = () => {
-    return <div></div>
-}
+  return (
+    <div className="w-full bg-sky-500/70 shadow shadow-sky-500/40 p-4">
+      <DesktopMenu />
+      <MobileMenu />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
